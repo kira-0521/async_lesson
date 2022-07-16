124237bffd5ae7a4e21becf7ebe3f67587fe1e4c
kira-0521 <kiratanaka0521@gmail.com>
2022-07-16 10:57:28 +0900

    fix

---
+++ final.js
@@ -0,0 +1,10 @@
+/*
+  ? １秒待ってから「global end」と表示される処理を書いてください
+  */
+
+/*
+  ? userId: 4に紐づく/postsの「title」を配列で出力してください。
+  ! ただし、userIdは/usersへリクエストを飛ばして取得してください。
+  /users: https://jsonplaceholder.typicode.com/users
+  /posts: https://jsonplaceholder.typicode.com/posts
+  */
